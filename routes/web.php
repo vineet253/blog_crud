2aa670fe86c0b2f378739d313a4cc0367125c5c4
vineet253 <39668738+vineet253@users.noreply.github.com>
2019-06-25 18:17:07 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -18,13 +18,13 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-
+Route::group(['middleware' => ['auth']], function () {
 Route::get('blog-list', 'blogController@index')->name('blog-list');
 Route::get('add-blog',  'blogController@add_blog')->name('add-blog');
 Route::post('add-blog', 'blogController@add_blog')->name('add-blog');
 Route::get('edit-blog/{id}',  'blogController@edit_blog')->name('edit-blog');
 Route::post('edit-blog/{id}',  'blogController@edit_blog')->name('edit-blog');
 Route::post('delete-blog/{id}',  'blogController@delete_blog')->name('delete-blog');
-
+});
 Route::get('font-blog', 'blogController@show')->name('font-blog');
 Route::get('blog/{id}',  'blogController@show_detail')->name('blog');
